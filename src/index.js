9e5c95f15dce3379eaae1cffb77ba9414f4a7615
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-01 22:41:13 +0100

    Adding more functions

--- src/index.js
+++ src/index.js
@@ -20,14 +20,26 @@ function Square(props) {
     }
   
     handleClick(i) {
-      const squares = this.state.squares.slice();
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
         return;
       }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
-        squares: squares,
+        history: history.concat([{
+          squares: squares,
+        }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
       });
     }
   
@@ -44,6 +56,16 @@ function Square(props) {
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ? 'Go to move #' + move: 'Go to game start';
+        return(
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        )
+      })
+
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
@@ -80,6 +102,7 @@ function Square(props) {
         history: [{
           squares: Array(9).fill(null),
         }],
+        stepNumber: 0,
         xIsNext: true,
       };
     }
@@ -95,7 +118,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
